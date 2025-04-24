eb67e62f16959e89c4ccea0c5dc2c3db01189cfb
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-04-24 10:25:12 +1000

    cors

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ import cron from "node-cron"
 import githubRoutes from "./src/routes/githubRoutes.js"
 import { fetchAndStoreGitHubData } from "./src/services/githubServices.js";
 import { connectDb } from "./src/config/mongoDb.js";
+import cors from "cors"
 
 dotenv.config();
 const app = express()
+app.use(cors({
+    origin: 'http://localhost:5173', // replace with your real frontend URL
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+
 const PORT = 9000;
 app.use(express.json())
 app.use("/api/v1/contribution", githubRoutes)
